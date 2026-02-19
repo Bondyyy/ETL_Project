6bb83237ca1d788dde9241caede601db84ca4bfc
Bondy <safaridung987@gmail.com>
2026-02-19 17:12:02 +0700

    validate schema sql

--- databases/schema_manager.py
+++ databases/schema_manager.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
 SQL_FILE_PATH = Path(__file__).parent.parent / 'sql' / 'schema.sql'
+
 def create_mysql_schema(connection, cursor):
     database = 'github_data'
 
-    cursor.execute(f"DROP DATABASE IF EXISTS {database}")
+    cursor.execute(f"DROP DATABASE IF EXISTS {database}") 
     cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database}")
-    cursor.commit()
+    connection.commit()
     print(f'Database {database} created successfully')
 
     connection.database = database
@@ -19,3 +20,30 @@ def create_mysql_schema(connection, cursor):
             if command:
                 cursor.execute(command)
     print('Schema created successfully')
+
+def validate_mysql_schema(cursor):
+    cursor.execute('SHOW TABLES')
+    # print(cursor.fetchall())
+    listTable = cursor.fetchall()
+    for table in listTable:
+        if 'users' not in table[0] and 'repositories' not in table[0]:
+            raise ValueError(f"Table {table} not found in database")
+    print('Schema validated successfully')
+    
+    cursor.execute('''
+                   SELECT * FROM users 
+                   WHERE user_id = 1
+                   ''')
+    user = cursor.fetchone()
+    if user is None:
+        raise ValueError('User not found in users table')
+    print('User found in users table')
+
+    cursor.execute('''
+                   SELECT * FROM repositories 
+                   WHERE repo_id = 1
+                   ''')
+    repository = cursor.fetchone()
+    if repository is None:
+        raise ValueError('Repository not found in repositories table')
+    print('Repository found in repositories table')
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from databases.mySqlConnect import MySQLConnect
 from config.databaseConfig import getDatabaseConfig
-from databases.schema_manager import create_mysql_schema
+from databases.schema_manager import create_mysql_schema, validate_mysql_schema
 
 def main(config):
     with MySQLConnect(config['mysql'].host, config['mysql'].port, 
                  config['mysql'].user, config['mysql'].password) as mySqlConnect:
         connection, cursor = mySqlConnect.connection, mySqlConnect.cursor
         create_mysql_schema(connection, cursor)
+        cursor.execute('INSERT INTO users (user_id, login, gravatar_id, url, avatar_url) VALUES (%s, %s, %s, %s, %s)', 
+                       (1, 'demo', '', 'https://demo.com', 'https://demo.com/avatar.com'))
+        cursor.execute('INSERT INTO repositories (repo_id, name, url) VALUES (%s, %s, %s)', (1, 'demo', 'https://demo.com'))
+        connection.commit()
+        validate_mysql_schema(cursor)
 
 if __name__ == "__main__":
     config = getDatabaseConfig()
